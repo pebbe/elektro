8979ffce8e6fe1ef8f062a7ea12418cbd3bbbb78
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2021-01-18 11:24:04 +0100

    write.ino: meer patronen

--- 01-morse/write/write.ino
+++ 01-morse/write/write.ino
@@ -5,6 +5,8 @@
 #define EEPROM_D7 12
 #define WRITE_EN 13
 
+#define PATTERN 32
+
 /*
  * Output the address bits and outputEnable signal using shift registers.
  */
@@ -24,13 +26,13 @@ void setAddress(int address, bool outputEnable) {
  */
 byte readEEPROM(int address) {
     for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
-      pinMode(pin, INPUT);
+	pinMode(pin, INPUT);
     }
     setAddress(address, /*outputEnable*/ true);
 
     byte data = 0;
     for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
-      data = (data << 1) + digitalRead(pin);
+	data = (data << 1) + digitalRead(pin);
     }
     return data;
 }
@@ -55,65 +57,123 @@ void writeEEPROM(int address, byte data) {
     delay(10);
 }
 
+#if PATTERN == 0
+
 byte data[] = {
-    0b0010,
-    0b0000,
-    0b0010,
-    0b0100,
-    0b0110,
-    0b0100,
-    0b1100,
-    0b1100,
-    0b1110,
-    0b1110,
-    0b1110,
-    0b1100,
-    0b1110,
-    0b1010,
-    0b1010,
-    0b1000,
-    0b0010,
-    0b0010,
-    0b0010,
-    0b0000,
-    0b0000,
-    0b0000,
-    0b0010,
-    0b0100,
-    0b0110,
-    0b0100,
-    0b1110,
-    0b1100,
-    0b1100,
-    0b1100,
-    0b1100,
-    0b1100,
-    0b1100,
-    0b1000,
-    0b1000,
-    0b1000,
-    0b0000,
-    0b0000,
-    0b0000,
-    0b0000
+	       0b00000100,
+	       0b00000000,
+	       0b00000100,
+	       0b00010000,
+	       0b00010100,
+	       0b00010000,
+	       0b01010000,
+	       0b01010000,
+	       0b01010100,
+	       0b01010100,
+	       0b01010100,
+	       0b01010000,
+	       0b01010100,
+	       0b01000100,
+	       0b01000100,
+	       0b01000000,
+	       0b00000100,
+	       0b00000100,
+	       0b00000100,
+	       0b00000000,
+	       0b00000000,
+	       0b00000000,
+	       0b00000100,
+	       0b00010000,
+	       0b00010100,
+	       0b00010000,
+	       0b01010100,
+	       0b01010000,
+	       0b01010000,
+	       0b01010000,
+	       0b01010000,
+	       0b01010000,
+	       0b01010000,
+	       0b01000000,
+	       0b01000000,
+	       0b01000000,
+	       0b00000000,
+	       0b00000000,
+	       0b00000000,
+	       0b00000000
 };
+
+#endif
+
+#if PATTERN == 1
+
+byte data[] = {
+	       0b10,
+	       0b00
+};
+
+#endif
+
+#if PATTERN == 2
+
+byte data[] = {
+	       0b100,
+	       0b000
+};
+
+#endif
+
+#if PATTERN == 4
+
+byte data[] = {
+	       0b1000,
+	       0b0000
+};
+
+#endif
+
+#if PATTERN == 8
+
+byte data[] = {
+	       0b10000,
+	       0b00000
+};
+
+#endif
+
+#if PATTERN == 16
+
+byte data[] = {
+	       0b100000,
+	       0b000000
+};
+
+#endif
+
+#if PATTERN == 32
+
+byte data[] = {
+	       0b1000000,
+	       0b0000000
+};
+
+#endif
 
 /*
  * Read the contents of the EEPROM and print them to the serial monitor.
  */
 void printContents() {
-  for (int base = 0; base < 1024; base += 16) {
-          byte data[16];
-    for (int offset = 0; offset <= 15; offset += 1) {
-      data[offset] = readEEPROM(base + offset);
-    }
+    for (int base = PATTERN << 7; base < (PATTERN << 7) + 128; base += 16) {
+	byte data[16];
+	for (int offset = 0; offset <= 15; offset += 1) {
+	    data[offset] = readEEPROM(base + offset);
+	}
 
-    char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
-      base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-      data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-    Serial.println(buf);
-  }
+	char buf[80];
+	sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+		base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+		data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+	Serial.println(buf);
+    }
 }
 
 void setup() {
@@ -129,7 +189,7 @@ void setup() {
     // Program data bytes
     Serial.print("Programming EEPROM");
     for (int address = 0; address < 128; address += 1) {
-        writeEEPROM(address, address < sizeof(data) ? data[address] : 1);
+        writeEEPROM(address + (PATTERN << 7), address < sizeof(data) ? data[address] : 1);
         if (address % 16 == 0) {
             Serial.print(".");
         }

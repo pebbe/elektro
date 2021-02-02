90743501f5fb4345f209faf15c117ea5fa2fcf40
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2021-02-02 13:19:09 +0100

    ATtiny85

---
+++ 02-tiny/signal/signal.ino
@@ -0,0 +1,31 @@
+// een sketch voor de ATtiny85, iets interessanter dan blink
+
+#define LOWER 20
+
+int pin[3]   = { 4, 0, 1 }; // fysieke pinnen met pwm: 3, 5, 6
+int level[3] = { 0, 0, 0 };
+int count[3] = { 2, 4, 8 };
+
+void setup() {
+  for (int i = 0; i < 3; i++) {
+    pinMode(pin[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    int set = 0;
+    if (count[i]-- == 0) {
+      count[i] = random(5, 500);
+      level[i] = random(level[i] < LOWER ? LOWER : level[i], 256);
+      set = 1;
+    } else if (level[i] > 0) {
+      level[i] = (6 * level[i]) / 7;
+      set = 1;
+    }
+    if (set) {
+      analogWrite(pin[i], level[i]);
+    }
+  }
+  delay(20);
+}

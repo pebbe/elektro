ece88f02546a691b55f315f2c80c0a836daa944b
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2021-02-03 12:10:26 +0100

    comments in 02-tiny/signal/signal.ino

--- 02-tiny/signal/signal.ino
+++ 02-tiny/signal/signal.ino
@@ -1,8 +1,8 @@
-// een sketch voor de ATtiny85, iets interessanter dan blink
+// een sketch voor de ATtiny85, iets interessanter dan Blink
 
 #define LOWER 20
 
-int pin[3]   = { 4, 0, 1 }; // fysieke pinnen met pwm: 3, 5, 6
+int pin[3]   = { 4, 0, 1 };    // fysieke pinnen met pwm: 3, 5, 6
 int level[3] = { 0, 0, 0 };
 int count[3] = { 2, 4, 8 };
 
@@ -20,7 +20,7 @@ void loop() {
       level[i] = random(level[i] < LOWER ? LOWER : level[i], 256);
       set = 1;
     } else if (level[i] > 0) {
-      level[i] = (6 * level[i]) / 7;
+      level[i] = (6 * level[i]) / 7;    // exponentionele uitdoving
       set = 1;
     }
     if (set) {

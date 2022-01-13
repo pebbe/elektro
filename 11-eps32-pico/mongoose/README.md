6d91c1886d874c529eab661ced023ea36e89da1a
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2022-01-13 17:45:12 +0100

    11: MongooseOS: FAIL!

--- 11-eps32-pico/mongoose/README.md
+++ 11-eps32-pico/mongoose/README.md
@@ -17,6 +17,30 @@ In `mos.yml` toevoegen onder `libs`:
 mos build --platform esp32
 mos flash
 mos wifi SSID PASSWORD
-mos config-set mqtt.enable=true mqtt.server=rpi-zero-2.fritz.box:1883 sntp.server=fritz.box
+mos config-set \
+  mqtt.enable=true \
+  mqtt.max_qos=1 \
+  mqtt.server=rpi-zero-2.fritz.box:1883 \
+  mqtt.will_message=---- \
+  mqtt.will_retain=true \
+  mqtt.will_topic=esp32/demo11/up \
+  sntp.server=fritz.box \
+  sys.tz_spec=CET-1CEST,M3.5.0/2,M10.5.0/3
 mos console
 ```
+
+## FAIL
+
+In MongooseOS zou je met JavaScript moeten kunnen werken, maar de
+implementatie is te gebrekkig. De keywords `var` en `case` zijn niet
+geïmplementeerd. Er is geen conversie tussen strings en
+getallen. Je kunt niet de inhoud van twee string-variabelen met
+elkaar vergelijken. De implementatie van subscribe in MQTT geeft als
+message een string zonder einde: blijkbaar verwacht de library dat de
+string met een nul-byte aan het einde verzonden worden. Als het een
+getal is verzonden als string is er geen moeglijkheid de ontvangen
+data weer om te zetten naar een getal.
+
+Je kunt ook in C/C++ werken, maar dan moet je na elke bewerking de
+boel op de server laten compileren, en het resultaten flashen. Dan kun
+je beter gewoon ArduinoIDE of Espressif gebruiken.
--- 11-eps32-pico/mongoose/demo/fs/init.js
+++ 11-eps32-pico/mongoose/demo/fs/init.js
@@ -1,16 +1,9 @@
-//load('api_aws.js');
-//load('api_azure.js');
-//load('api_config.js');
-//load('api_dash.js');
-//load('api_events.js');
-//load('api_gcp.js');
 load('api_gpio.js');
-//load('api_mqtt.js');
-//load('api_shadow.js');
-load('api_timer.js');
-//load('api_sys.js');
-//load('api_watson.js');
+load('api_log.js');
+load('api_math.js');
+load('api_mqtt.js');
 load('api_pwm.js');
+load('api_timer.js');
 
 function blink(ms) {
   let id = Timer.set(ms, Timer.REPEAT, function() {
@@ -21,6 +14,7 @@ function blink(ms) {
   return id;
 }
 
+
 GPIO.set_mode(25, GPIO.MODE_OUTPUT);
 GPIO.set_mode(26, GPIO.MODE_OUTPUT);
 GPIO.set_mode(27, GPIO.MODE_OUTPUT);
@@ -30,8 +24,48 @@ let mytimer = blink(1000);
 
 PWM.set(27, 5000, 0.25);
 
+// MQTT
 
-Timer.set(10000, 0, function() {
-  Timer.del(mytimer);
-  mytimer = blink(500);
+function handleMQTT(conn, topic, msg) {
+  Log.info(topic);
+  Log.info(msg);
+  let value = Math.round(msg);
+  if (topic == 'esp32/demo11/blink') {
+    if (value < 1) {
+      value = 1;
+    }
+    Timer.del(mytimer);
+    mytimer = blink(value * 100);
+  } else if (topic == 'esp32/demo11/level') {
+    if (value < 0) {
+      value = 0;
+    } else if (value > 255) {
+      value = 255;
+    }
+    PWM.set(27, 5000, value / 255);
+  }
+}
+
+function setupMQTT() {
+  Log.info('setupMQTT')
+  MQTT.pub('esp32/demo11/up', 'up!', 1, true);
+  MQTT.sub('esp32/demo11/blink', handleMQTT);
+  MQTT.sub('esp32/demo11/level', handleMQTT);
+}
+
+let t1 = Timer.set(10, Timer.REPEAT, function() {
+  if (MQTT.isConnected() && Timer.now() > 100000000) {
+    Timer.del(t1);
+    setupMQTT();
+  }
 }, null);
+
+
+// Print time each 20 sec
+
+function printTime() {
+  Log.info(Timer.fmt('%A %e %B %Y, %H:%M:%S %Z', Timer.now()));
+}
+
+printTime();
+Timer.set(20000, Timer.REPEAT, printTime, null);
